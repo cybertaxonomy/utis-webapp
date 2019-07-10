0470eeaceba072402467027f47378119613b7405
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2019-07-10 22:10:28 +0200

    better log folder detection

--- src/main/java/org/bgbm/utis/logging/ContextDependentInitializer.java
+++ src/main/java/org/bgbm/utis/logging/ContextDependentInitializer.java
@@ -8,9 +8,15 @@
 */
 package org.bgbm.utis.logging;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * @author a.kohlbecker
@@ -23,7 +29,34 @@ public class ContextDependentInitializer implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
         ServletContext context = event.getServletContext();
         String contextPath = context.getContextPath();
-        System.setProperty("contextPath", contextPath.isEmpty() ? "rootContext" : contextPath);
+        // in production the utis webapp will have a context path, therefore the default is names utis-dev
+        System.setProperty("contextPath", contextPath.isEmpty() ? "utis-dev" : contextPath);
+
+        if(System.getProperty("logFolder") == null){
+            List<File> folderCandidates = new ArrayList<>();
+            folderCandidates.add(new File("/var/log/utis"));
+            if(System.getProperty("jetty.home") != null){
+                folderCandidates.add(new File(System.getProperty("jetty.home"), "log"));
+            }
+            if(System.getProperty("cataline.home") != null){
+                folderCandidates.add(new File(System.getProperty("cataline.home"), "log"));
+            }
+            folderCandidates.add(new File(FileUtils.getUserDirectory(), ".utis"));
+
+            for(File f : folderCandidates){
+                if(!f.exists()){
+                    if(!f.mkdirs()){
+                        continue;
+                    } else {
+
+                    }
+                }
+                if(f.canWrite()){
+                    System.setProperty("logFolder", f.getPath());
+                    break;
+                }
+            }
+        }
     }
 
     /**
